8b6891f3f9458c825a29c50fc8def17687f852a3
Akiyama <tetares4@gmail.com>
2017-09-05 17:49:23 +0900

    リザルトマネージャーなどの追加

---
+++ Assets/Main/Script/ResultManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ResultManager : MonoBehaviour {
+
+    private ScoreType score = ScoreType.SCORE_D;
+    public ScoreType Score { get { return score; } }
+
+    private List<string> comment = new List<string>();
+    public string[] Comment { get { return comment.ToArray(); } }
+
+    public int GemCount { get { return 0; } }
+    public int DamageCount { get { return 0; } }
+    public int TeleportCount { get { return 0; } }
+
+    private List<SpotControl> spots = new List<SpotControl>();
+    public SpotControl[] Spots { get { return spots.ToArray(); } }
+
+    public enum ScoreType
+    {
+        SCORE_D,
+        SCORE_C,
+        SCORE_B,
+        SCORE_A,
+        SCORE_S,
+        SCORE_SS,
+    }
+
+    void Start()
+    {
+        comment.Add("yeah");
+
+        spots.Add(new SpotControl());
+    }
+}
---
+++ Assets/Main/Script/SpotControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotControl : MonoBehaviour {
+
+    private bool reached;
+    public bool IsReached { get { return reached; } }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        reached = true;
+        GetComponent<Collider>().enabled = false;
+    }
+}
